f02d83771bf0f2c8dda94f0e5e4e2453b791e08b
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-06-05 11:43:21 +0700

    implement Restaurant Item

--- lib/widgets/restaunrant.dart
+++ lib/widgets/restaunrant.dart
@@ -1,37 +1,106 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
 
 class RestaurantItem extends StatelessWidget {
   final String name;
   final String address;
-  final double star;
+  final double rating;
   final Widget image;
   const RestaurantItem({
     Key key,
     this.name,
     this.address,
-    this.star,
+    this.rating,
     this.image,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        left: 10,
-        right: 10,
-      ),
-      child: Opacity(
-        opacity: 1,
-        child: Stack(
+    return InkWell(
+      onTap: () {
+        print('Choose restaunrant $name');
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 10,
+          right: 10,
+        ),
+        child: Column(
           children: <Widget>[
             ClipRRect(
-              borderRadius: BorderRadius.circular(15),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(5),
+                topRight: Radius.circular(5),
+              ),
               child: Opacity(
-                opacity: 0.7,
+                opacity: 1,
                 child: image,
               ),
             ),
+            Container(
+              height: 50,
+              width: 300,
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(5),
+                  bottomRight: Radius.circular(5),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 7,
+                    offset: Offset(0, 4),
+                  )
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text(
+                    name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              EvilIcons.location,
+                              color: Colors.orange,
+                            ),
+                            Text(
+                              address,
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 12,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SmoothStarRating(
+                        allowHalfRating: true,
+                        color: Colors.yellow,
+                        rating: rating,
+                        isReadOnly: true,
+                        starCount: 5,
+                        size: 18,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
