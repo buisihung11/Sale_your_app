e79b2cd7aa4192bdbb4317948afba5f4c08aa938
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-06-05 11:38:08 +0700

    Add top restaurant section

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:sale_your_food/widgets/foodItem.dart';
 import 'package:sale_your_food/widgets/homeCarousel.dart';
+import 'package:sale_your_food/widgets/restaunrant.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -10,6 +11,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: SingleChildScrollView(
           padding: EdgeInsets.all(10),
@@ -25,21 +27,31 @@ class HomeScreen extends StatelessWidget {
               HomeCarousel(),
               // Categories
               Categories(),
-              SizedBox(
-                height: 15
-              ),
+              SizedBox(height: 15),
               // Food near you
               Align(
                 alignment: Alignment.topLeft,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(
-                      'Food near you',
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Food near you',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            'View All',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ],
                       ),
                     ),
                     SizedBox(height: 10),
@@ -97,61 +109,79 @@ class HomeScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(
-                      'Top Restaurant',
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                    SizedBox(height: 15),
                     Container(
-                      height: 160,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
-                          FoodItem(
-                            name: 'Bread',
-                            image: Image.asset(
-                              'assets/images/bread.jpg',
-                              width: 150,
-                              height: 150,
-                              fit: BoxFit.fill,
+                          Text(
+                            'Top Restaurant',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
                             ),
-                            availableItem: 1,
-                            discount: 0.45,
-                            price: 10,
                           ),
-                          FoodItem(
-                            name: 'Cake',
-                            image: Image.asset(
-                              'assets/images/cake.jpg',
-                              width: 150,
-                              height: 150,
-                              fit: BoxFit.fill,
-                            ),
-                            availableItem: 0,
-                            discount: 0.45,
-                            price: 20,
-                          ),
-                          FoodItem(
-                            name: 'Water bottle',
-                            image: Image.asset(
-                              'assets/images/water_bottle.jpg',
-                              width: 150,
-                              height: 150,
-                              fit: BoxFit.fill,
-                            ),
-                            availableItem: 1,
-                            discount: 0.35,
-                            price: 10,
+                          Text(
+                            'View All',
+                            style: TextStyle(color: Colors.blue),
                           ),
                         ],
                       ),
                     ),
+                    SizedBox(height: 15),
+                    Container(
+                      height: 240,
+                      margin: const EdgeInsets.only(
+                        top: 10,
+                        bottom: 10,
+                      ),
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          RestaurantItem(
+                            name: 'Vinmart',
+                            image: Image.asset(
+                              'assets/images/restaurant1.jpg',
+                              width: 300,
+                              height: 170,
+                              fit: BoxFit.cover,
+                            ),
+                            address: 'Tay Hoa, Q9',
+                            rating: 5,
+                          ),
+                          RestaurantItem(
+                            name: 'Vietnamese Restaurant',
+                            image: Image.asset(
+                              'assets/images/restaurant2.jpg',
+                              width: 300,
+                              height: 170,
+                              fit: BoxFit.cover,
+                            ),
+                            address: 'Dien Bien Phu, Q.Binh Thanh',
+                            rating: 4.5,
+                          ),
+                          RestaurantItem(
+                            name: 'Bread',
+                            image: Image.asset(
+                              'assets/images/restaurant3.jpg',
+                              width: 300,
+                              height: 170,
+                              fit: BoxFit.cover,
+                            ),
+                            address: 'Le Van Viet, Q9',
+                            rating: 3.5,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 15),
                   ],
                 ),
+              ),
+
+              SizedBox(
+                height: 15,
               ),
             ],
           ),
