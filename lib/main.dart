3261ccb6e0abf1fe8dd017d32002bf96be5a3116
KhanhTD <thanlongcr12@gmail.com>
2020-06-12 06:35:04 +0700

    Khanh commit profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:sale_your_food/screens/login.dart';
 
+const kSpacingUnit = 10;
+
+const kDarkPrimaryColor = Color(0xFF212121);
+const kDarkSecondaryColor = Color(0xFF373737);
+const kLightPrimaryColor = Color(0xFFFFFFFF);
+const kLightSecondaryColor = Color(0xFFF3F7FB);
+const kAccentColor = Color(0xFFFFC107);
+
 void main() {
   runApp(MyApp());
 }
+  
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -12,8 +21,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Roboto',
+theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'RobotoMono',
+        primaryColor: kLightPrimaryColor,
+        canvasColor: kLightPrimaryColor,
+        backgroundColor: kLightSecondaryColor,
+        accentColor: kAccentColor,
+        iconTheme: ThemeData.dark().iconTheme.copyWith(
+              color: kDarkSecondaryColor,
+            ),
+        textTheme: ThemeData.dark().textTheme.apply(
+              fontFamily: 'RobotoMono',
+              bodyColor: kDarkSecondaryColor,
+              displayColor: kDarkSecondaryColor,
+            ),
         primarySwatch: Colors.blue,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
