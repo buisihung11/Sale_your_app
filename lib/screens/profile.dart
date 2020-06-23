4a1a4be412593ec283f516a863c082d5e898f28c
KhanhTD <thanlongcr12@gmail.com>
2020-06-24 00:01:08 +0700

    update profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,102 +1,176 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
-import 'package:sale_your_food/constants.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(context, height: 896, width: 414, allowFontScaling: true);
-    var profileInfo = Expanded(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: kSpacingUnit.w * 10,
-            width: kSpacingUnit.w * 10,
-            margin: EdgeInsets.only(top: kSpacingUnit.w * 3),
-            child: Stack(
+    return Scaffold(
+      backgroundColor: Color(0xff09031D),
+      body: Padding(
+        padding: const EdgeInsets.only(top:25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                CircleAvatar(
-                  radius: kSpacingUnit.w * 5,
-                  backgroundImage: AssetImage('assets/images/cake.jpg'),
+                Padding(
+                  padding: const EdgeInsets.only(left: 28.0, top: 7.0),
+                  child: CircleAvatar(
+                    radius: 40,
+                    backgroundImage: AssetImage('assets/images/cake.jpg'),
+                  ),
                 ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Container(
-                    height: kSpacingUnit.w * 2.5,
-                    width: kSpacingUnit.w * 2.5,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).accentColor,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Icon(
-                      LineAwesomeIcons.pen,
-                      color: kDarkTheme.primaryColor,
-                      size: ScreenUtil().setSp(kSpacingUnit.w * 1.5),
-                    ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 27.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'KhanhTD',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.location_on,
+                                color: Colors.white, size: 17),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Text(
+                                'FPT University',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  wordSpacing: 2,
+                                  letterSpacing: 4,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-          ),
-          SizedBox(height: kSpacingUnit.w * 2),
-          Text('Nicolas Tesla', style: kTitleTextStyle),
-          SizedBox(height: kSpacingUnit.w * 0.5),
-          Text('Nicolas@gmail.com', style: kCaptionTextStyle),
-        ],
-      ),
-    );
-
-    var header = Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        profileInfo,
-      ],
-    );
-
-    return Theme(
-      data: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'RobotoMono',
-        primaryColor: kLightPrimaryColor,
-        canvasColor: kLightPrimaryColor,
-        backgroundColor: kLightSecondaryColor,
-        accentColor: kAccentColor,
-        iconTheme: ThemeData.dark().iconTheme.copyWith(
-              color: kDarkSecondaryColor,
-            ),
-        textTheme: ThemeData.dark().textTheme.apply(
-              fontFamily: 'RobotoMono',
-              bodyColor: kDarkSecondaryColor,
-              displayColor: kDarkSecondaryColor,
-            ),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      child: Scaffold(
-        body: Column(
-          children: <Widget>[
-            SizedBox(height: kSpacingUnit.w * 4),
-            header,
-            Expanded(
-              child: ListView(
+            Padding(
+              padding: const EdgeInsets.fromLTRB(38, 15, 38, 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  ProfileListItem(
-                      icon: LineAwesomeIcons.history, text: 'Purchase History'),
-                  ProfileListItem(
-                      icon: LineAwesomeIcons.filter, text: 'Filter'),
-                  ProfileListItem(
-                      icon: LineAwesomeIcons.question_circle,
-                      text: 'Help & Support'),
-                  ProfileListItem(icon: LineAwesomeIcons.cog, text: 'Settings'),
-                  ProfileListItem(
-                    icon: LineAwesomeIcons.alternate_sign_out,
-                    text: 'Logout',
-                    hasNavigation: false,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '17',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'items',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: 0.2,
+                    height: 22,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '10Kg',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'save',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: 0.2,
+                    height: 22,
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.only(left: 18, right: 18, top: 8, bottom: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(33)),
+                      gradient: LinearGradient(
+                        colors: [Color(0xff6D0EB5), Color(0xff4059F1)],
+                        begin: Alignment.bottomRight,
+                        end: Alignment.centerLeft,
+                      ),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        // Icon(Icons.,
+                        //     color: Colors.white, size: 17),
+                        Text(
+                          'ranking',
+                          style: TextStyle(
+                            color: Colors.white,
+                            wordSpacing: 2,
+                            letterSpacing: 4,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(top: 15),
+                decoration: BoxDecoration(
+                  color: Color(0xffEFEFEF),
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(34)),
+                ),
+                child: ListView(
+                  children: <Widget>[
+                    ProfileItem(
+                        icon: LineAwesomeIcons.history, text: 'Purchase History'),
+                    ProfileItem(
+                        icon: LineAwesomeIcons.filter, text: 'Filter'),
+                    ProfileItem(
+                        icon: LineAwesomeIcons.question_circle,
+                        text: 'Help & Support'),
+                    ProfileItem(icon: LineAwesomeIcons.cog, text: 'Settings'),
+                    ProfileItem(
+                      icon: LineAwesomeIcons.alternate_sign_out,
+                      text: 'Logout',
+                      hasNavigation: false,
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
@@ -106,11 +180,11 @@ class ProfileScreen extends StatelessWidget {
   }
 }
 
-class ProfileListItem extends StatelessWidget {
+class ProfileItem extends StatelessWidget {
   final IconData icon;
   final text;
   final bool hasNavigation;
-  const ProfileListItem({
+  const ProfileItem({
     Key key,
     this.icon,
     this.text,
@@ -119,46 +193,54 @@ class ProfileListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: kSpacingUnit.w * 5,
-      margin: EdgeInsets.symmetric(
-        horizontal: kSpacingUnit.w * 4,
-      ).copyWith(
-        bottom: kSpacingUnit.w * 2,
-      ),
-      padding: EdgeInsets.symmetric(
-        horizontal: kSpacingUnit.w * 2,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(kSpacingUnit.w * 3),
-        color: Theme.of(context).backgroundColor,
-      ),
-      child: InkWell(
-        onTap: () {
-          print(this.text);
-        },
-        child: Row(
-          children: <Widget>[
-            Icon(
-              this.icon,
-              size: kSpacingUnit.w * 2.5,
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 6, left: 15, right: 15),
+      child: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+              top: BorderSide(color: Colors.grey[100]),
+              bottom: BorderSide(color: Colors.grey[100]),
+              left: BorderSide(color: Colors.grey[100]),
+              right: BorderSide(color: Colors.grey[100])),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(255, 140, 5, .2),
+              blurRadius: 20,
+              offset: Offset(0, 7),
             ),
-            SizedBox(
-              width: kSpacingUnit.w * 2.5,
-            ),
-            Text(
-              this.text,
-              style: kTitleTextStyle.copyWith(
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            Spacer(),
-            if (this.hasNavigation)
-              Icon(
-                LineAwesomeIcons.angle_right,
-                size: kSpacingUnit.w * 2.5,
-              ),
           ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 15),
+          child: Row(
+            children: <Widget>[
+              Icon(
+                this.icon,
+                size: 25,
+                color: Colors.grey[600],
+              ),
+              SizedBox(
+                width: 25,
+              ),
+              Text(
+                this.text,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[600],
+                ),
+              ),
+              Spacer(),
+              if (this.hasNavigation)
+                Icon(
+                  LineAwesomeIcons.angle_right,
+                  size: 25,
+                  color: Colors.grey[600],
+                ),
+            ],
+          ),
         ),
       ),
     );
