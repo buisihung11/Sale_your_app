bae03791e2d84517e8c8301486827ecc3f12a2cd
KhanhTD <thanlongcr12@gmail.com>
2020-06-24 14:56:25 +0700

    update profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -7,137 +7,74 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff09031D),
-      body: Padding(
-        padding: const EdgeInsets.only(top:25),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 28.0, top: 7.0),
-                  child: CircleAvatar(
-                    radius: 40,
-                    backgroundImage: AssetImage('assets/images/cake.jpg'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 27.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'KhanhTD',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 28,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8.0),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(Icons.location_on,
-                                color: Colors.white, size: 17),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 8.0),
-                              child: Text(
-                                'FPT University',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  wordSpacing: 2,
-                                  letterSpacing: 4,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(38, 15, 38, 12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      backgroundColor: Color.fromRGBO(255, 51, 0, 1),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bg_profile.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        '17',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                        ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 28.0, top: 7.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromRGBO(50, 20, 50, .8),
+                            blurRadius: 15,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
                       ),
-                      Text(
-                        'items',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    color: Colors.white,
-                    width: 0.2,
-                    height: 22,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        '10Kg',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'save',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    color: Colors.white,
-                    width: 0.2,
-                    height: 22,
-                  ),
-                  Container(
-                    padding:
-                        EdgeInsets.only(left: 18, right: 18, top: 8, bottom: 5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(33)),
-                      gradient: LinearGradient(
-                        colors: [Color(0xff6D0EB5), Color(0xff4059F1)],
-                        begin: Alignment.bottomRight,
-                        end: Alignment.centerLeft,
+                      child: CircleAvatar(
+                        radius: 40,
+                        backgroundImage: AssetImage('assets/images/cake.jpg'),
                       ),
                     ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 27.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        // Icon(Icons.,
-                        //     color: Colors.white, size: 17),
                         Text(
-                          'ranking',
+                          'KhanhTD',
                           style: TextStyle(
-                            color: Colors.white,
-                            wordSpacing: 2,
-                            letterSpacing: 4,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 28,
+                            color: Colors.grey[800],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.location_on,
+                                  color: Colors.red[800], size: 17),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0),
+                                child: Text(
+                                  'FPT University',
+                                  style: TextStyle(
+                                    color: Colors.grey[700],
+                                    wordSpacing: 1,
+                                    letterSpacing: 2,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
@@ -145,35 +82,121 @@ class ProfileScreen extends StatelessWidget {
                   ),
                 ],
               ),
-            ),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                margin: EdgeInsets.only(top: 15),
-                decoration: BoxDecoration(
-                  color: Color(0xffEFEFEF),
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(34)),
-                ),
-                child: ListView(
+              Padding(
+                padding: const EdgeInsets.fromLTRB(38, 15, 38, 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    ProfileItem(
-                        icon: LineAwesomeIcons.history, text: 'Purchase History'),
-                    ProfileItem(
-                        icon: LineAwesomeIcons.filter, text: 'Filter'),
-                    ProfileItem(
-                        icon: LineAwesomeIcons.question_circle,
-                        text: 'Help & Support'),
-                    ProfileItem(icon: LineAwesomeIcons.cog, text: 'Settings'),
-                    ProfileItem(
-                      icon: LineAwesomeIcons.alternate_sign_out,
-                      text: 'Logout',
-                      hasNavigation: false,
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '17',
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'items',
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      color: Colors.grey[800],
+                      width: 0.2,
+                      height: 22,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '10Kg',
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                            fontSize: 45,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'save',
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                            fontSize: 22,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      color: Colors.grey[800],
+                      width: 0.2,
+                      height: 22,
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(
+                          left: 18, right: 18, top: 8, bottom: 5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(33)),
+                        gradient: LinearGradient(
+                          colors: [Color.fromRGBO(255, 10, 20, 1), Color.fromRGBO(255, 191, 0, 1)],
+                          begin: Alignment.bottomRight,
+                          end: Alignment.centerLeft,
+                        ),
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          // Image.asset(''),
+                          Text(
+                            'Iron-Man',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              wordSpacing: 1,
+                              letterSpacing: 2,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
-            ),
-          ],
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 15),
+                  decoration: BoxDecoration(
+                    color: Color(0xffEFEFEF),
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(34)),
+                  ),
+                  child: ListView(
+                    children: <Widget>[
+                      ProfileItem(
+                          icon: LineAwesomeIcons.history,
+                          text: 'Purchase History'),
+                      ProfileItem(
+                          icon: LineAwesomeIcons.filter, text: 'Filter'),
+                      ProfileItem(
+                          icon: LineAwesomeIcons.question_circle,
+                          text: 'Help & Support'),
+                      ProfileItem(icon: LineAwesomeIcons.cog, text: 'Settings'),
+                      ProfileItem(
+                        icon: LineAwesomeIcons.alternate_sign_out,
+                        text: 'Logout',
+                        hasNavigation: false,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
